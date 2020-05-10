0f1423896d627a642c5878e32b48ffc5e33e6124
Seth Bailey <seth.bailey@students.hartismere.com>
2020-05-10 11:43:11 +0100

    Generalising the way I add colours by adding concepts, also adding gitignore

---
+++ Colours.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace game
+{
+    internal class Colours
+    {
+
+
+        static public ConsoleColor GoldReward {get { return ConsoleColor.Yellow;}}
+        static public ConsoleColor Gold {get { return ConsoleColor.DarkYellow;}}
+        static public ConsoleColor Damage {get { return ConsoleColor.DarkRed;}}
+        public static ConsoleColor Health { get { return ConsoleColor.Green;}}
+        public static ConsoleColor Medicine { get { return ConsoleColor.DarkGreen;}}
+        public static ConsoleColor Attack { get { return ConsoleColor.DarkCyan;}}
+        public static ConsoleColor Armour { get {return ConsoleColor.DarkBlue;}}
+        public static ConsoleColor Protection { get {return ConsoleColor.Blue;}}
+        public static ConsoleColor Speech { get {return ConsoleColor.White;}}
+        public static ConsoleColor Cotton { get {return ConsoleColor.Magenta;}}
+        public static ConsoleColor FightDamage { get {return ConsoleColor.Red;}}
+    }
+}
